c7978b1b0ce52fa38aed2dc53a3a7b5e0007d626
lenovo <792545884@qq.com>
2019-10-22 00:24:47 +0800

    前端

--- xfz/front/src/js/index.js
+++ xfz/front/src/js/index.js
@@ -29,10 +29,66 @@
 
 // 控制轮播图
 function Banner(){
+    // 监听轮播图
+    this.bannerGroup = $("#banner-group")
+    //每次改变的时候就会保存，如果只是在下面的方法定义不在此定义的话，每次都会从第一个图开始循环
+    this.index = 0
+    // 一但对象被初始化之后就监听banner
+
+    //直接定义left-arrow
+    this.leftArrow = $('.left-arrow')
+    this.rightArrow = $('.right-arrow')
+    this.listenBannerHover()
 
 }
 
-Banner.prototype.run = function(){
+// 控制左右箭头是否需要隐藏，鼠标移动到图片区域就显示，否则隐藏
+Banner.prototype.toggleArrow = function (isShow) {
+    var self = this
+    if (isShow){
+        // 直接调用show方法
+        // $('.left-arrow').show()
+        // $('.right-arrow').show()
+
+        //优化方法
+        // 但是这样做的话每次移动进出都要寻找left-arrow right-arrow，消耗时间
+        // 可以直接在类中定义，方法中直接调用就好
+        self.leftArrow.show()
+        self.rightArrow.show()
+    }else{
+        // $('.left-arrow').hide()
+        // $('.right-arrow').hide()
+
+        //优化之后
+        self.leftArrow.hide()
+        self.rightArrow.hide()
+    }
+}
+
+// 监听鼠标移动事件
+Banner.prototype.listenBannerHover = function () {
+    // this表示Banner类,
+    var self = this
+    // 鼠标移动事件
+    this.bannerGroup.hover(function () {
+        //第一个函数是， 把鼠标移动到banner上会执行的函数
+        // 停止轮播图，停止执行 timer
+        clearInterval(self.timer)
+        //显示左右箭头
+        self.toggleArrow(true)
+
+    }, function (){
+        //第二个函数是， 把鼠标从banner上移走会执行的函数
+        self.loop()
+        // 隐藏左右箭头
+        self.toggleArrow(false)
+    })
+}
+
+Banner.prototype.loop = function(){
+    // 在此定义的this表示Banner类,在改类的方法中调用类属性
+    var self = this
+
     // 获取所有id=banner-ul的元素
     var bannerUl = $("#banner-ul")
     // 修改left值，一步到位，再次打开网页的时候还是该图片而不是原本的第一张
@@ -40,7 +96,27 @@ Banner.prototype.run = function(){
     // bannerUl.css({"left": -798})
 
     // 产生动画效果, 还可以指定过度时间，500毫秒
-    bannerUl.animate({"left": -798}, 500)
+    // bannerUl.animate({"left": -798}, 500)
+
+    // 根据下标轮播
+    // var index = 0
+    // 计时器，把需要执行的函数放进去即可
+    this.timer = setInterval(function () {
+        if (self.index >= 3){
+            self.index = 0
+        }else{
+            self.index += 1
+        }
+        bannerUl.animate({"left": -798 * self.index}, 500)
+    }, 2000) //2秒轮播一次
+
+}
+
+
+Banner.prototype.run = function () {
+    console.log("running")
+    this.loop()
+
 }
 
 // 全部文本执行之后再执行此代码
