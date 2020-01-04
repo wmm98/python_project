1a818efd7605779338be2839a0cdfc59c5aadf7c
lenovo <792545884@qq.com>
2020-01-04 23:31:16 +0800

    前端

--- xfz/front/src/js/index.js
+++ xfz/front/src/js/index.js
@@ -29,17 +29,56 @@
 
 // 控制轮播图
 function Banner(){
+    this.bannerWidth = 798
     // 监听轮播图
     this.bannerGroup = $("#banner-group")
     //每次改变的时候就会保存，如果只是在下面的方法定义不在此定义的话，每次都会从第一个图开始循环
-    this.index = 0
-    // 一但对象被初始化之后就监听banner
+    this.index = 1
 
     //直接定义left-arrow
     this.leftArrow = $('.left-arrow')
     this.rightArrow = $('.right-arrow')
-    this.listenBannerHover()
 
+    //获取banner-ul下有多少个li标签，即多少个图片，不用写死
+    this.bannerUl = $("#banner-ul")
+    this.liList = this.bannerUl.children("li")
+    this.bannerCount = this.liList.length
+    this.pageControl = $(".page-control")
+
+
+
+}
+
+// js动态修改轮播图
+Banner.prototype.initBanner = function(){
+    var self = this
+    var firstBanner = self.liList.eq(0).clone()
+    var lastBanner = self.liList.eq(self.bannerCount-1).clone()
+    self.bannerUl.append(firstBanner)
+    self.bannerUl.prepend(lastBanner)
+    // 修改css样式
+    self.bannerUl.css({"width": self.bannerWidth*(self.bannerCount+2),
+    'left': -self.bannerWidth})
+
+}
+
+
+// 根据轮播图的个数动态修改小圆点的结构和样式
+Banner.prototype.initPageControl = function(){
+    var self = this;
+
+    for (var i = 0; i<self.bannerCount; i++ ){
+        // 动态生成li标签，小点点
+        var circle = $("<li></li>")
+        self.pageControl.append(circle)
+        if(i == 0){
+            // 给css增加属性，第一个白色填充
+            circle.addClass("active")
+        }
+
+    }
+    // 动态修改小圆点样式
+    self.pageControl.css({"width": self.bannerCount*12 + 8*2 + 16*(self.bannerCount - 1)})
 }
 
 // 控制左右箭头是否需要隐藏，鼠标移动到图片区域就显示，否则隐藏
@@ -85,12 +124,31 @@ Banner.prototype.listenBannerHover = function () {
     })
 }
 
+// 重复利用轮播图
+Banner.prototype.animate = function() {
+    var self = this
+    self.bannerUl.animate({"left": -798 * self.index}, 500)
+    var index = self.index
+    if (index == 0){
+        index = self.bannerCount-1
+    }else if(index == self.bannerCount+1){
+        index = 0
+
+    }else{
+        index = self.index - 1
+    }
+    self.pageControl.children('li').eq(index).addClass("active").siblings().
+    removeClass('active')
+
+}
+
+//计时器
 Banner.prototype.loop = function(){
     // 在此定义的this表示Banner类,在改类的方法中调用类属性
     var self = this
 
     // 获取所有id=banner-ul的元素
-    var bannerUl = $("#banner-ul")
+    // var bannerUl = $("#banner-ul")
     // 修改left值，一步到位，再次打开网页的时候还是该图片而不是原本的第一张
     // 不适合
     // bannerUl.css({"left": -798})
@@ -102,20 +160,72 @@ Banner.prototype.loop = function(){
     // var index = 0
     // 计时器，把需要执行的函数放进去即可
     this.timer = setInterval(function () {
-        if (self.index >= 3){
-            self.index = 0
+        if (self.index >= self.bannerCount + 1){
+            self.bannerUl.css({"left":-self.bannerWidth})
+            self.index = 2
         }else{
-            self.index += 1
+            self.index ++
         }
-        bannerUl.animate({"left": -798 * self.index}, 500)
+        self.animate()
     }, 2000) //2秒轮播一次
 
 }
 
+// 点击箭头上下切换图片
+Banner.prototype.listenArrowClick = function (){
+    var self = this
+    // 点击左箭头
+    self.leftArrow.click(function (){
+        if(self.index == 0){
+            // ==：1==‘1’返回true
+            //====: 1 != '1' false 1==1 true
+            self.bannerUl.css({"left":-self.bannerCount*self.bannerWidth})
+            self.index = self.bannerCount - 1
+        }else{
+            self.index--
+        }
+        // 条件满足就实现轮播
+        self.animate()
+    })
+
+    // 右边
+    self.rightArrow.click(function (){
+        if(self.index == self.bannerCount + 1){
+            self.bannerUl.css({"left":-self.bannerWidth})
+            self.index = 2
+        }else{
+            self.index++
+        }
+        // 条件满足就实现轮播
+        self.animate()
+    })
+
+}
+
+
+//小圆点的轮播
+Banner.prototype.listenPageControl = function(){
+    var self = this
+    self.pageControl.children("li").each(function(index, obj){
+        $(obj).click(function(){
+            self.index = index
+            //移除没有选中的圆点状态
+            $(obj).addClass("active").siblings().removeClass("active")
+            self.animate()
+        })
+    })
+}
 
 Banner.prototype.run = function () {
     console.log("running")
+    this.initPageControl()
+    this.initBanner()
     this.loop()
+    this.listenArrowClick()
+    // 一但对象被初始化之后就监听banner
+    this.listenBannerHover()
+    this.listenPageControl()
+
 
 }
 
